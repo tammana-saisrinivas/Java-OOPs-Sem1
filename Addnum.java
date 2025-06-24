dcc216c73d38d973f15e2f2aa42f0ffb1cb4a9e3
TAMMANA SAISRINIVAS <saisrinivastammana@gmail.com>
2025-06-24 11:05:05 +0530

    Add Numbers

---
+++ Addnum.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Addnum{
+    public static void main(String args[])
+    
+    { Scanner sc=new Scanner(System.in);
+
+System.out.println("Enter the value of a=");
+int a=sc.nextInt();
+
+System.out.println("Enter the value of b=");
+int b=sc.nextInt();
+System.out.println("The sum is ="+(a+b));
+    }
+}
