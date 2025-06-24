668b88be840587f599c94beaba86e7baad323bd9
TAMMANA SAISRINIVAS <saisrinivastammana@gmail.com>
2025-06-24 20:32:57 +0530

    Adding String Codes

---
+++ Stringsc.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Stringsc {
+public static void main(String args[]){
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter the String :");
+String str=sc.nextLine();
+System.out.println("Hello "+str);
+
+}
+}
