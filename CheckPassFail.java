a7a2b1c28fc3b4c54bfe9943ea7410579e3bba60
TAMMANA SAISRINIVAS <saisrinivastammana@gmail.com>
2025-06-25 20:27:43 +0530

    Added Check Pass or Fail Java Codes

---
+++ CheckPassFail.java
@@ -0,0 +1,12 @@
+class CheckPassFail{
+    public static void main(String args[]){
+        int mark=80;
+        System.out.println("MARK:"+mark);
+        if (mark>=50){
+            System.out.println("Pass");
+        }else{
+            System.out.println("Fail"); }
+        System.out.println("Done");   
+        
+    }
+}
